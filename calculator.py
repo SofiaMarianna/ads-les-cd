bcbfe1f3ae8ba2dd6acfe692da569cba6730b065
SofiaMarianna <sofiamariannalisboa@gmail.com>
2026-06-09 16:44:00 -0300

    Ajuste de cálculo de divisão por zero

--- calculator.py
+++ calculator.py
@@ -4,5 +4,7 @@ def subtracao(a,b):
     return a-b
 def multiplicacao(a,b): 
     return a*b
-def divisao(a,b):
-    return a/b
+def divisao(a, b):
+    if b == 0:
+        raise ValueError("Divisão por zero")
+    return a / b
